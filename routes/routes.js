aca94f4c03d5dbc4619e540a8a5d8b8424c736d9
Simon X <simonxiong11@gmail.com>
2018-06-05 12:52:57 -0700

    added createUser route

--- routes/routes.js
+++ routes/routes.js
@@ -3,4 +3,9 @@ module.exports = function(app) {
   app.get('/', function(req, res){
     res.send('it works');
   })
+
+  app.get('/createUser', function(req, res){
+    res.render('createUser');
+  })
+
 }
