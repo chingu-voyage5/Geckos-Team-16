251e411e4b8bf209850b3bf091dd7c2069da9e28
Simon X <simonxiong11@gmail.com>
2018-06-05 12:58:10 -0700

    require and configure PATH

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const express = require('express'),
       app = express(),
       bodyParser = require('body-parser');
       // mongoose = require('mongoose');
+      PATH = require('path');
 
 const routeController = require('./routes/routes.js');
 
 // const dburl = 'mongodb://localhost/chirpper';
 // mongoose.connect(dburl);
 
+//App configurations
 app.set('view engine', 'ejs');
+app.set('views', PATH.join(__dirname, 'Views')); //I think this sets the default directory for views. Do we need this? Look up. --jared
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
