cec1227391a3caabe0ed96dd89a1f1bb5ff5c6fa
Tiffany Lam <tlam5@sbcglobal.net>
2018-06-18 15:14:11 -0700

    Associate users to chirps and vice versa

--- models/chirp.js
+++ models/chirp.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 //Schema Config
 var chirpSchema = new mongoose.Schema({
     body: String,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
     //emoji: ?
     //photo: ?
 }); 
