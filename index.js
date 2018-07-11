c0a275d8c61e7caaddb91a31bf9c03575a528780
Jared <jaredpschiller@gmail.com>
2018-07-10 19:28:24 -0500

    restored bodyParser.json line on index file

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ mongoose.connect(dburl);
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Passport configurations
